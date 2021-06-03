abdf379d49ecd2378d3f465d0c4c3b476524d28d
Laura <laura@HYD-Admins-MacBook-Pro.local>
2021-06-03 18:11:16 +0200

    timeout added to pollers - live QA cookie check

--- src/helpers/pollForTrue.js
+++ src/helpers/pollForTrue.js
@@ -1,27 +1,26 @@
 /**
- * 
  * @param {query: string} - an elements selector
  * @param {timeOut: number} - time in ms after which the poller will stop looking for the element
  * 
- * @return {element: HTMLelement} - if eventually found, returns the element
- * 
  */
 
-export default (query, timeOutMs = 15000) => {
+export default (cb, timeOutMs = 15000) => {
     
-    if (typeof query !== 'string')  new Error (`${query} is not a string`);
-
+    if (typeof cb !== 'function') return new Error (`${cb} is not a function`);
+    if (typeof timeOutMs !== 'number') return new Error (`${timeOutMs} is not a number`);
+    
     return new Promise((resolve) => {
-
+        
         let timeOutCount = 0
         let interval = setInterval(()=>timeOutCount = timeOutCount+100, 100)
-
+        
         const wfelem = () => {
-
+            
             if(timeOutCount > timeOutMs){ clearInterval(interval); return }
-
-            (null != document.querySelector(query))
-                ? (resolve(document.querySelector(query)))
+            console.log('cb: ', cb());
+            
+            (cb())
+                ? resolve(true)
                 : window.requestAnimationFrame(wfelem);
 
         };
@@ -30,3 +29,4 @@ export default (query, timeOutMs = 15000) => {
     });
 
 }
+
